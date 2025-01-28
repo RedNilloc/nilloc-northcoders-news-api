e5e526c482a22e883ac4546e1f43e35bd401e479
RedNilloc <coregond11@gmail.com>
2025-01-28 11:29:50 +0000

    add my completion of task 2

---
+++ controllers/controller.js
@@ -0,0 +1,10 @@
+const app = require("../app.js");
+const endpoints = require("../endpoints.json")
+
+
+const getEndpoints = (request, response) => {
+    response.status(200).send({ endpoints });  
+
+}
+
+module.exports = { getEndpoints }
---
+++ listen.js
@@ -0,0 +1,12 @@
+
+
+
+const app = require("./app");
+const PORT = 3000;
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Listening on ${PORT}!`);
+  }
+});
