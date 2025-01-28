13722e1c199cf938a091f12797f4db28dbf2deb3
RedNilloc <coregond11@gmail.com>
2025-01-28 16:27:07 +0000

    add my progress in task 3

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,17 @@
 const endpointsJson = require("../endpoints.json");
 const request = require("supertest");
+const seed = require("../db/seeds/seed.js")
 const app = require("../app.js");
+const db = require("../db/connection.js");
 const testData = require("../db/data/test-data/index.js");
+
+beforeEach(()=> {
+  return seed(testData);
+});
+
+afterAll(() => {
+return db.end();
+}); 
 
 /* Set up your test imports here */
 
@@ -13,8 +23,21 @@ describe("GET /api", () => {
       .get("/api")
       .expect(200)
       .then((response) => {
-        console.log(response.body)
+        //console.log(response.body)
         expect(response.body.endpoints).toEqual(endpointsJson);
       });
+  });
+});
+
+describe("GET /api/topics", () => {
+  test("GET:200 sends an array of topics to the client", () => {
+    return request(app)
+    .get('/api/topics')
+    .expect(200)
+    .then((response) => {
+      console.log(response.body)
+
+     });
+ 
   });
 });
