8bab43d781a760cbb39148cec995e5ff94f78227
RedNilloc <coregond11@gmail.com>
2025-01-31 15:57:58 +0000

    add some additional changes trying to get hosting working

--- listen.js
+++ listen.js
@@ -1,10 +1,13 @@
 const app = require("./app.js");
-const { PORT = 9090 } = process.env
+const { PORT = 9090 } = process.env;
 
-app.listen(PORT, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`Listening on ${PORT}!`);
-  }
-});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+
+// app.listen(PORT, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(`Listening on ${PORT}!`);
+//   }
+// });
