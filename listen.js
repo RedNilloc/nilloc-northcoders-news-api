b41038881debe5f2c3b05c732c3a281406bbf8e6
RedNilloc <coregond11@gmail.com>
2025-01-31 15:29:54 +0000

    add my changes to set up hosting

--- listen.js
+++ listen.js
@@ -1,5 +1,6 @@
-const app = require("./app");
-const PORT = 3000;
+const app = require("./app.js");
+const { PORT = 9090 } = process.env
+
 app.listen(PORT, (err) => {
   if (err) {
     console.log(err);
